6f19881006417d44e7e0812ee8f0fd8739f52aa2
Huan Wang <fredwanghuan@gmail.com>
2025-07-18 18:53:50 -0600

    first impl

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/fredwangwang/test-codecov-go/pkg/fib"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	fmt.Println(fib.FibRecursive(10))
+	// fmt.Println(fib.FibIterative(10))
+	// fmt.Println(fib.FibMemoization(10))
+}
---
+++ pkg/fib/fib.go
@@ -0,0 +1,21 @@
+package fib
+
+func FibRecursive(n int) int {
+	if n <= 1 {
+		return n
+	}
+	return FibRecursive(n-1) + FibRecursive(n-2)
+}
+
+func FibIterative(n int) int {
+	if n <= 1 {
+		return n
+	}
+	a, b := 0, 1
+	for i := 2; i <= n; i++ {
+		a, b = b, a+b
+	}
+	return b
+}
+
+func FibMemoization(n int) int { return 0 }
